59e4622d30db7564bf27c90f7882c4ff74dde94c
harryhmohney <harrison.mohney@gmail.com>
2023-07-25 12:00:06 -0700

    fixed display

--- main.js
+++ main.js
@@ -1,97 +1,276 @@
 
-function startGame() {
-
-}
-
-function nextQuestion() {
-
-}
-
-function selectAnswer() {
-  
-}
-
-// create object
-
-const questions = [
-  {
-    question0: 'What css rule is used for changing any HTML element into a flexbox container?',
-    options: ['flexbox', 'grid', 'container', 'display'],
-    correctAnswer: 3
-  },
-  {
-    question1: 'What is the functionality of the "mv" command in the terminal?',
-    options: ['move', 'copy', 'rename', 'all of the above'],
-    correctAnswer: 3
-  },
-  {
-    question2: 'Which array property is used in `for loop` for iterating through the entire list?',
-    options: ['.stretch', '.indexOf', '.length', '.forEach'],
-    correctAnswer: 2
-  },
-  {
-    question3: 'What is the method of adding and/or omitting an index in an array?'
-    options: ['.push()', '.splice()', '.pop()', '.shift()'],
-    correctAnswer: 1
-  },
-  {
-    question4: 'A functions (answer1) are passed in when the function is invoked, it differs from (answer2) because they are variables declared in the function definition an used within the function body when invoked.',
-    options: ['parameter, argument', 'argument, callback', 'parameter, return', 'argument, parameter'],
-    correctAnswer: 3
-  },
-  {
-    question5: 'What are an object`s `properties` made of?',
-    options: ['key: value', 'value: key', 'array: string', 'value: property'],
-    correctAnswer: 0
-  },
-  {
-    question6: 'How would you select all <a> elements contained within a div with an id of globalNav?',
-    options: ['document.querySelector(“#globalNav")', 'document.querySelectorAll(“#globalNav a”)', 'element.querySelectorAll(“#a”)', 'document.getElementById(“#globalNav a”)'],
-    correctAnswer: 1
-  },
-  {
-    question7: 'An application that revolves around the management and manipulation of data as its primary focus is what?',
-    options: ['object-oriented', 'DOM-manipulation', 'data-centric', 'data-oriented'],
-    correctAnswer: 2
-  },
-  {
-    question8: 'Refers to the concept of bundling data and the functions that operate on that data within a single unit.',
-    options: ['object oriented programming', 'DOM', 'array', 'DRY code'],
-    correctAnswer: 0
-  },
-  {
-    question9: 'A way to create blueprints for objects that share common properties and behavior.',
-    options: ['CSS', 'JS classes', '<div>', 'pseudocode'],
-    correctAnswer: 1
-  }
-
-];
-function showQuestion(mv) {
+function showQuestion(question) {
   // select DOM element
-  let titleDiv = document.getElementById('title');
+  let containEl = document.getElementById('q-contain');
   // modify it
-  titleDiv.textContent = mv.title;
-  // select li class
-  let ops = document.querySelectorAll('.options');
+  containEl.textContent = question.question;
+  
+  let answer = document.querySelectorAll('.answer');
 
-  ops.forEach(function(el, idx) {
-    el.textContent = mv.options[idx];
+  answer.forEach(function(btn, idx) {
+    // access the text of each answer option
+    btn.textContent = question.options[idx].text;
 
-    el.addEventListener('click', function() {
-      // check correct option
-      if (mv.correctAnswer == idx) {
+    btn.addEventListener('click', function() {
+      // check correct answer
+      if (question.options[idx].correct) {
         console.log('Correct!');
       } else {
         console.log('So close!')
       }
     });
   });
-}
+};
 
-showQuestion(question1);
+// questions array
+const questions = [
+  {
+    question: 'What css rule is used for changing any HTML element into a flexbox container?',
+    options: [
+      {
+        text: 'flexbox',
+        correct: false
+      },
+      {
+        text: 'grid',
+        correct: false
+      },
+      {
+        text: 'container',
+        correct: false
+      },
+      {
+        text: 'display',
+        correct: true
+      }
+    ]
+  },
+  {
+    question: 'What is the functionality of the "mv" command in the terminal?',
+    options: [
+      {
+        text: 'move',
+        correct: false
+      },
+      {
+        text: 'copy',
+        correct: false
+      },
+      {
+        text: 'rename',
+        correct: false
+      },
+      {
+        text: 'all of the above',
+        correct: true
+      },
+    ]
+  },
+  {
+    question: 'What css rule is used for changing any HTML element into a flexbox container?',
+    options: [
+      {
+        text: 'flexbox',
+        correct: false
+      },
+      {
+        text: 'grid',
+        correct: false
+      },
+      {
+        text: 'container',
+        correct: false
+      },
+      {
+        text: 'display',
+        correct: true
+      }
+    ]
+  },
+  {
+    question: 'Which array property is used in `for loop` for iterating through the entire list?',
+    options: [
+      {
+        text: '.stretch',
+        correct: false
+      },
+      {
+        text: '.indexOf',
+        correct: false
+      },
+      {
+        text: '.length',
+        correct: true
+      },
+      {
+        text: '.forEach',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'What is the method of adding and/or omitting an index in an array?',
+    options: [
+      {
+        text: '.push()',
+        correct: false
+      },
+      {
+        text: '.splice()',
+        correct: true
+      },
+      {
+        text: '.pop()',
+        correct: false
+      },
+      {
+        text: '.shift()',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'A functions (answer1) are passed in when the function is invoked, it differs from (answer2) because they are variables declared in the function definition an used within the function body when invoked.',
+    options: [
+      {
+        text: 'parameter, argument',
+        correct: false
+      },
+      {
+        text: 'argument, callback',
+        correct: false
+      },
+      {
+        text: 'parameter, return',
+        correct: false
+      },
+      {
+        text: 'argument, parameter',
+        correct: true
+      }
+    ]
+  },
+  {
+    question: 'What are an object`s `properties` made of?',
+    options: [
+      {
+        text: 'key: value',
+        correct: true
+      },
+      {
+        text: 'value: key',
+        correct: false
+      },
+      {
+        text: 'array: string',
+        correct: false
+      },
+      {
+        text: 'value: property',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'How would you select all <a> elements contained within a div with an id of globalNav?',
+    options: [
+      {
+        text: 'document.querySelector("#globalNav")',
+        correct: false
+      },
+      {
+        text: 'document.querySelectorAll("#globalNav a")',
+        correct: true
+      },
+      {
+        text: 'element.querySelectorAll("#a")',
+        correct: false
+      },
+      {
+        text: 'document.getElementById("#globalNav a")',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'An application that revolves around the management and manipulation of data as its primary focus is what?',
+    options: [
+      {
+        text: 'object-oriented',
+        correct: false
+      },
+      {
+        text: 'DOM-manipulation',
+        correct: false
+      },
+      {
+        text: 'data-centric',
+        correct: true
+      },
+      {
+        text: 'data-oriented',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Refers to the concept of bundling data and the functions that operate on that data within a single unit.',
+    options: [
+      {
+        text: 'object oriented programming',
+        correct: true
+      },
+      {
+        text: 'DOM',
+        correct: false
+      },
+      {
+        text: 'array',
+        correct: false
+      },
+      {
+        text: 'DRY code',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'A way to create blueprints for objects that share common properties and behavior.',
+    options: [
+      {
+        text: 'CSS',
+        correct: false
+      },
+      {
+        text: 'JS classes',
+        correct: true
+      },
+      {
+        text: '<div>',
+        correct: false
+      },
+      {
+        text: 'pseudocode',
+        correct: false
+      }
+    ]
+  }
+];
+      
+// keep track of the current question
+let currentQuestionIndex = 0; 
 
-let btn = document.getElementById('btn');
+showQuestion(questions[currentQuestionIndex]);
 
-btn.addEventListener('click', function() {
-  console.log('clicked');
+let nextBtn = document.getElementById('next-btn');
+
+nextBtn.addEventListener('click', function() {
+  // iterate to the next question
+  currentQuestionIndex++;
+  if (currentQuestionIndex <= questions.length) {
+    showQuestion(questions[currentQuestionIndex]);
+  } else {
+    // if all questions have been answered
+    console.log('End of the quiz, GREAT JOB!'); 
+  }
 });
